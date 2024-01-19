29cb423d38f2296624a5f92a65c564e368b35d82
adnan <AdnanGhannam2001>
2024-01-19 14:37:58 +0300

    Add RMQ Library
    NOTE: Still not tested yet

---
+++ libs/rmq/src/rmq.module.ts
@@ -0,0 +1,28 @@
+import { DynamicModule, Module } from '@nestjs/common';
+import { RmqService } from './rmq.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+
+@Module({
+  providers: [RmqService],
+  exports: [RmqService],
+})
+export class RmqModule {
+  static register(name: string): DynamicModule {
+    return {
+      module: RmqModule,
+      imports: [
+        ClientsModule.register([
+          {
+            name,
+            transport: Transport.RMQ,
+            options: {
+              queue: `LEARN_IO_${name}_QUEUE`,
+              urls: ['amqp://localhost:5672']
+            }
+          }
+        ])
+      ],
+      exports: [ClientsModule]
+    }
+  }
+}
---
+++ libs/rmq/src/rmq.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+@Injectable()
+export class RmqService {
+    configurations(name: string, noAck: boolean = false): MicroserviceOptions {
+        return {
+            transport: Transport.RMQ,
+            options: {
+              queue: `LEARN_IO_${name}_QUEUE`,
+              urls: ['amqp://localhost:5672'],
+              noAck
+            }
+        }
+    }
+}
